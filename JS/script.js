6b9a9382eb961f50cbf7789793e1d01220eecbde
Brendan Zankowski <brendiesel5@gmail.com>
2024-07-10 15:16:13 -0700

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -142,7 +142,7 @@ async function get_albums_and_songs(){
     if (album["name"].includes("Exclusive")) continue;
     if (album["name"].includes("Orchestration")) continue;
     if (album["name"].includes("Edited")) continue;
-    if (album["name"].includes("Gimme a Second 2")) continue;
+    if (album["name"].includes("Gimme A Second 2")) continue;
     if (album["name"].includes("No Face")) continue;
     if (album["name"].includes("CARNIVAL")) continue;
     if (album["name"].includes("TALKING")) continue;
